9172f7b3312f4ea887f1b834e3f6bc49c2b25fff
matthewdavis <matt.t.davis@outlook.com>
2020-01-05 23:37:07 -0500

    Third attempt

--- lc7/7process.py
+++ lc7/7process.py
@@ -1,4 +1,15 @@
 def reverse(x):
-    return(int(str(x)[::-1]))
+    num = str(abs(x))
+    if x < 0:
+        result = -1 * int(num[::-1])
+    else:
+        result = int(num[::-1])
+    if result not in range((-1 << 31), (1 << 31) - 1):
+        return 0
 
-reverse(123)
+    return result
+
+print(reverse(123))
+print(reverse(-123))
+print(reverse(120))
+print(reverse(1534236469))
